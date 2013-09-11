356ff58690d74bed4c203ab0789f39183dd3a80c
Ian Li <i@techotaku.net>
2013-09-11 12:15:16 +0800

    Remove deployment scripts

--- test/configure.sh
+++
@@ -1,3 +0,0 @@
-php ./test/make-fpm-cfg.php $USER `pwd`/.deployment/php-fpm.sock > ./.deployment/php-fpm.conf
-php ./test/make-nginx-cfg.php `pwd` `pwd`/.deployment/php-fpm.sock > ./.deployment/nginx.conf
-cp ./.deployment/nginx.conf ~/openresty/nginx/conf/nginx.conf
--- test/install-nginx.sh
+++
@@ -1,8 +0,0 @@
-cd .deployment
-wget http://openresty.org/download/ngx_openresty-1.4.2.1.tar.gz
-tar xzf ngx_openresty-1.4.2.1.tar.gz
-cd ngx_openresty-1.4.2.1
-./configure --prefix=$HOME/openresty --with-luajit
-make
-make install
-cd ../..
